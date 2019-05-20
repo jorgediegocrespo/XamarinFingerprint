d9ca1526c4dfc168d4265bc4ed369fed3e9762dc
jorgediegocrespo <jorgediegocrespo@gmail.com>
2019-05-20 18:32:07 +0200

    Base project

---
+++ LectorHuega/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace LectorHuega
+{
+    using Plugin.Fingerprint;
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnAuthenticate_Clicked(object sender, System.EventArgs e)
+        {
+            var result = await CrossFingerprint.Current.AuthenticateAsync("Tap that fingerprint sensor!");
+            if (result.Authenticated)
+            {
+                LblResult.Text = "VALID fingerprint";
+                LblResult.TextColor = Color.Green;
+            }
+            else
+            {
+                LblResult.Text = "INVALID fingerprint";
+                LblResult.TextColor = Color.Red;
+            }
+        }
+    }
+}
